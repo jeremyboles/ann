039af2e36133c163d71bf8098faf358321249920
Jeremy Boles <me@jeremyboles.com>
2022-03-24 05:00:31 -0500

    feat(foil sigh): add a website header

--- apps/foilsigh/lib/foilsigh/views/home_view.ex
+++ apps/foilsigh/lib/foilsigh/views/home_view.ex
@@ -1,6 +1,7 @@
 defmodule Foilsigh.HomeView do
   use Foilsigh, :view
 
-  def stylesheets(_action, _assigns), do: []
+  def stylesheets(_action, _assigns), do: ["/assets/home/index.css"]
+
   def title(_action, _assigns), do: "Jeremy Boles"
 end
