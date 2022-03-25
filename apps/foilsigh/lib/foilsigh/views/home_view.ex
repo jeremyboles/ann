aedd8e0819468a1869c0f2725911364c6ac02bf0
Jeremy Boles <me@jeremyboles.com>
2022-03-25 10:35:12 -0500

    feat(foilsigh): add the index page for the wiki

--- apps/foilsigh/lib/foilsigh/views/home_view.ex
+++ apps/foilsigh/lib/foilsigh/views/home_view.ex
@@ -1,7 +1,7 @@
 defmodule Foilsigh.HomeView do
   use Foilsigh, :view
 
-  def stylesheets(_action, _assigns), do: ["/assets/home/index.css"]
+  def stylesheets(_action, _assigns), do: ~w(/assets/home/index.css)
 
   def title(_action, _assigns), do: "Jeremy Boles"
 end
--- apps/foilsigh/lib/foilsigh/views/wiki_view.ex
+++ apps/foilsigh/lib/foilsigh/views/wiki_view.ex
@@ -1,6 +1,8 @@
 defmodule Foilsigh.WikiView do
   use Foilsigh, :view
 
+  def stylesheets("index.html", _assigns), do: ~w(/assets/wiki/index.css)
   def stylesheets(_action, _assigns), do: []
-  def title(_action, _assigns), do: "Wiki · Jeremy Boles"
+
+  def title("index.html", _assigns), do: "Wiki · Jeremy Boles"
 end
