2c7604d02e90adcf21e3806170ec0b46f1913386
Jeremy Boles <me@jeremyboles.com>
2022-03-29 17:18:19 -0500

    style(bainistigh): remove some of left over comments from development

--- apps/bainistigh/lib/bainistigh/controllers/token_controller.ex
+++ apps/bainistigh/lib/bainistigh/controllers/token_controller.ex
@@ -2,13 +2,7 @@ defmodule Bainistigh.TokenController do
   use Bainistigh, :controller
 
   def show(conn, _params) do
-    # keyfile = Application.get_env(:bainistigh, __MODULE__)[:keyfile]
-    origin = Application.get_env(:bainistigh, Bainistigh.Endpoint)[:url][:host]
-
-    # signer = Joken.Signer.create("HS256", keyfile, %{"kid" => @key_id})
-    # {:ok, token} = Joken.Signer.sign(%{"iss" => @team_id, "origin" => origin}, signer)
-    token = Bainistigh.Token.generate_and_sign!(%{"origin" => origin})
-
+    token = Bainistigh.Token.generate_and_sign!()
     text(conn, token)
   end
 end
--- apps/bainistigh/lib/bainistigh/token.ex
+++ apps/bainistigh/lib/bainistigh/token.ex
@@ -3,7 +3,7 @@ defmodule Bainistigh.Token do
 
   def token_config do
     origin = Application.get_env(:bainistigh, Bainistigh.Endpoint)[:url][:host]
-    
+
     default_claims(default_exp: 30 * 60, iss: "6VW2HTV37Z", skip: [:aud])
     |> add_claim("origin", fn -> origin end, &(&1 == origin))
   end
