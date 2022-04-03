ec01f9f55c04bb7ac1ad47c85cdf43531c5f128d
Jeremy Boles <me@jeremyboles.com>
2022-04-03 10:43:04 -0500

    feat(bainistigh): add UI for setting the parent topic of an article

--- apps/taifead/lib/taifead/wiki/article.ex
+++ apps/taifead/lib/taifead/wiki/article.ex
@@ -8,18 +8,18 @@ defmodule Taifead.Wiki.Article do
   @timestamps_opts [type: :utc_datetime_usec]
 
   schema "articles" do
-    field :content_html, :string
-    field :content_text, :string
-    field :doc, :string
-    field :short_title, :string
-    field :tags, {:array, :string}
-    field :title_html, :string
-    field :title_text, :string
-    field :url_slug, :string
-    field :visibility, Ecto.Enum, default: :private, values: [:private, :published]
+    field(:content_html, :string)
+    field(:content_text, :string)
+    field(:doc, :string)
+    field(:short_title, :string)
+    field(:tags, {:array, :string})
+    field(:title_html, :string)
+    field(:title_text, :string)
+    field(:url_slug, :string)
+    field(:visibility, Ecto.Enum, default: :private, values: [:private, :published])
 
-    belongs_to :parent, __MODULE__, foreign_key: :parent_id
-    has_many :children, __MODULE__, foreign_key: :parent_id
+    belongs_to(:parent, __MODULE__, foreign_key: :parent_id)
+    has_many(:children, __MODULE__, foreign_key: :parent_id)
 
     timestamps()
   end
