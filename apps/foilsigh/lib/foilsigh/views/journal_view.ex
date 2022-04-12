e98e0ae76fd36db801048446370d62f21638e01e
Jeremy Boles <me@jeremyboles.com>
2022-04-12 11:02:24 -0500

    feat(foilsigh): start a design for the journal index

--- apps/foilsigh/lib/foilsigh/views/journal_view.ex
+++ apps/foilsigh/lib/foilsigh/views/journal_view.ex
@@ -1,7 +1,7 @@
 defmodule Foilsigh.JournalView do
   use Foilsigh, :view
 
-  def stylesheets(_action, _assigns), do: []
+  def stylesheets("index.html", _assigns), do: ~w(/assets/journal/index.css)
 
   def title(_action, _assigns), do: "Journal · Jeremy Boles"
 end
