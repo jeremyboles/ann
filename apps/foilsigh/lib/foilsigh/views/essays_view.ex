911f85b53c86da467e1c28280331ccac2c58f2d6
Jeremy Boles <me@jeremyboles.com>
2022-04-13 17:08:43 -0500

    feat(foilsigh): add a design for the essays index page

--- apps/foilsigh/lib/foilsigh/views/essays_view.ex
+++ apps/foilsigh/lib/foilsigh/views/essays_view.ex
@@ -1,6 +1,7 @@
 defmodule Foilsigh.EssaysView do
   use Foilsigh, :view
 
-  def stylesheets(_action, _assigns), do: []
+  def stylesheets("index.html", _assigns), do: ~w(/assets/essays/index.css)
+
   def title(_action, _assigns), do: "Essays · Jeremy Boles"
 end
