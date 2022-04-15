55a2bcd2fdffdb358c78fc2759528a78219d814a
Jeremy Boles <me@jeremyboles.com>
2022-04-15 14:24:19 -0500

    perf(foilsigh): optimize and minify CSS output

--- apps/foilsigh/assets/postcss.config.js
+++ apps/foilsigh/assets/postcss.config.js
@@ -1,8 +1,6 @@
 module.exports = () => ({
   plugins: [
-    require('postcss-preset-env')({
-      browsers: ['last 1 version'],
-      // stage: 3,
-    }),
+    require('postcss-preset-env')({ browsers: ['last 1 version'] }),
+    require('postcss-csso')({ forceMediaMerge: true }),
   ],
 })
