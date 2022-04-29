bc70de7ec761157179c2ce7b2ab2ecc4e28c78ab
Jeremy Boles <me@jeremyboles.com>
2022-04-29 13:15:49 -0500

    feat(foilsigh): add a design for the map page

--- apps/foilsigh/lib/foilsigh/components/graphics_component.ex
+++ apps/foilsigh/lib/foilsigh/components/graphics_component.ex
@@ -3,8 +3,17 @@ defmodule Foilsigh.GraphicsComponent do
   
   def map(%{inner_block: _} = assigns) do
     ~H"""
-      <svg role="img" viewBox={"0 0 #{@width} #{@height}"}>
+      <svg class="map" role="img" viewBox={"0 0 #{@width} #{@height}"}>
         <desc><%= render_slot(@inner_block) %></desc>
+        <use href={"/g/map.svg?height=#{@height}&width=#{@width}#map"}/>
+        <use href={"/g/points.svg?locations[recent][]=9ytetsdz2&locations[previous][]=spz3rj21d&locations[previous][]=gcvpq24ye&height=#{@height}&width=#{@width}#points"}/>
+      </svg>
+    """
+  end
+  
+  def map(assigns) do
+    ~H"""
+      <svg class="map" role="img" viewBox={"0 0 #{@width} #{@height}"}>
         <use href={"/g/map.svg?height=#{@height}&width=#{@width}#map"}/>
         <use href={"/g/points.svg?locations[recent][]=9ytetsdz2&locations[previous][]=spz3rj21d&locations[previous][]=gcvpq24ye&height=#{@height}&width=#{@width}#points"}/>
       </svg>
--- apps/foilsigh/lib/foilsigh/components/map_component.ex
+++ apps/foilsigh/lib/foilsigh/components/map_component.ex
@@ -1,3 +1,81 @@
 defmodule Foilsigh.MapComponent do
   use Foilsigh, :component
+
+  import Foilsigh.GraphicsComponent
+
+  def locations(assigns) do
+    ~H"""
+      <dl class="locations">
+        <div>
+          <dt>Belgium</dt>
+          <dd>
+            <a href="/map/gcyby"><b>Ath</b> <i>1 Post</i></a>
+          </dd>
+        </div>
+      
+        <div>
+          <dt>United States</dt>
+          <dd><a href="/map/gcyby"><b>Ath</b> <i>1 Post</i></a></dd>
+          <dd><a href="/map/gcyby"><b>Boston</b> <i>1 Post</i></a></dd>
+          <dd><a href="/map/gcyby"><b>New York City</b> <i>1 Post</i></a></dd>
+          <dd><a href="/map/gcyby"><b>Springfield, MO</b> <i>1 Post</i></a></dd>
+          <dd><a href="/map/gcyby"><b>Washington, DC</b> <i>1 Post</i></a></dd>
+        </div>
+      
+                <div>
+          <dt>Belgium</dt>
+          <dd>
+            <a href="/map/gcyby"><b>Ath</b> <i>1 Post</i></a>
+          </dd>
+        </div>
+      
+        <div>
+          <dt>United States</dt>
+          <dd><a href="/map/gcyby"><b>Ath</b> <i>1 Post</i></a></dd>
+          <dd><a href="/map/gcyby"><b>Boston</b> <i>1 Post</i></a></dd>
+          <dd><a href="/map/gcyby"><b>New York City</b> <i>1 Post</i></a></dd>
+          <dd><a href="/map/gcyby"><b>Springfield, MO</b> <i>1 Post</i></a></dd>
+          <dd><a href="/map/gcyby"><b>Washington, DC</b> <i>1 Post</i></a></dd>
+        </div>
+      
+                <div>
+          <dt>Belgium</dt>
+          <dd>
+            <a href="/map/gcyby"><b>Ath</b> <i>1 Post</i></a>
+          </dd>
+        </div>
+      
+        <div>
+          <dt>United States</dt>
+          <dd><a href="/map/gcyby"><b>Ath</b> <i>1 Post</i></a></dd>
+          <dd><a href="/map/gcyby"><b>Boston</b> <i>1 Post</i></a></dd>
+          <dd><a href="/map/gcyby"><b>New York City</b> <i>1 Post</i></a></dd>
+          <dd><a href="/map/gcyby"><b>Springfield, MO</b> <i>1 Post</i></a></dd>
+          <dd><a href="/map/gcyby"><b>Washington, DC</b> <i>1 Post</i></a></dd>
+        </div>
+      
+                <div>
+          <dt>Belgium</dt>
+          <dd>
+            <a href="/map/gcyby"><b>Ath</b> <i>1 Post</i></a>
+          </dd>
+        </div>
+      
+        <div>
+          <dt>United States</dt>
+          <dd><a href="/map/gcyby"><b>Ath</b> <i>1 Post</i></a></dd>
+          <dd><a href="/map/gcyby"><b>Boston</b> <i>1 Post</i></a></dd>
+          <dd><a href="/map/gcyby"><b>New York City</b> <i>1 Post</i></a></dd>
+          <dd><a href="/map/gcyby"><b>Springfield, MO</b> <i>1 Post</i></a></dd>
+          <dd><a href="/map/gcyby"><b>Washington, DC</b> <i>1 Post</i></a></dd>
+        </div>
+      </dl>
+    """
+  end
+
+  def world(assigns) do
+    ~H"""
+      <.map height="736" width="1680"/>
+    """
+  end
 end
