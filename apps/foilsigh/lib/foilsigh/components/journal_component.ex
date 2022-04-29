1358accd330e2434b982aca5b79769e80707cebf
Jeremy Boles <me@jeremyboles.com>
2022-04-29 06:12:20 -0500

    feat(foilsigh): finalize a design for the journal index

--- apps/foilsigh/lib/foilsigh/components/journal_component.ex
+++ apps/foilsigh/lib/foilsigh/components/journal_component.ex
@@ -20,6 +20,22 @@ defmodule Foilsigh.JournalComponent do
     """
   end
 
+  def entry_summary(%{type: type} = assigns) do
+    ~H"""
+      <article class={"entry_summary #{type}"}>
+        <p>
+          <span class="vh">On</span> <time datetime="2013-06-13 12:00:00">Dec 2<span class="vh">,</span> 2014 <span class="vh">@</span> 5:53 am</time><span class="vh">,</span>
+          <a class="vh" href="/">I</a> <a href={"/journal/#{type}"}>posted a <%= type %></a>
+          <span class="vh">from</span> <span><span>Paris</span><span class="vh">,</span> <abbr title="France">FR</abbr></span><span class="vh">:</span>
+        </p>
+      
+        <blockquote cite={"/journal/#{type}"}>
+          <.content type={type} />
+        </blockquote>
+      </article>
+    """
+  end
+
   def filter_by_type(assigns) do
     ~H"""
       <form class="filter_by_type">
@@ -101,166 +117,78 @@ defmodule Foilsigh.JournalComponent do
           <li><a href="/tags/foraging" rel="tag">foraging</a></li>
           <li><a href="/tags/garum" rel="tag">garum</a></li>
           <li><a href="/tags/fish-sauce" rel="tag">fish-sauce</a></li>
-          <li><a href="/tags/technology" rel="tag">technology</a></li>
-          <li><a href="/tags/itjustworks" rel="tag">itjustworks</a></li>
-          <li><a href="/tags/wine-caps" rel="tag">wine-caps</a></li>
-          <li><a href="/tags/wine-cap" rel="tag">wine-cap</a></li>
-          <li><a href="/tags/apple" rel="tag">apple</a></li>
-          <li><a href="/tags/morning" rel="tag">morning</a></li>
-          <li><a href="/tags/pinephone" rel="tag">pinephone</a></li>
-          <li><a href="/tags/gemini" rel="tag">gemini</a></li>
-          <li><a href="/tags/content" rel="tag">content</a></li>
-          <li><a href="/tags/indieweb" rel="tag">indieweb</a></li>
-          <li><a href="/tags/writing" rel="tag">writing</a></li>
-          <li><a href="/tags/focus" rel="tag">focus</a></li>
-          <li><a href="/tags/covid-19" rel="tag">covid-19</a></li>
-          <li><a href="/tags/friends" rel="tag">friends</a></li>
-          <li><a href="/tags/goals" rel="tag">goals</a></li>
-          <li><a href="/tags/css" rel="tag">css</a></li>
-          <li><a href="/tags/grid" rel="tag">grid</a></li>
-          <li><a href="/tags/layout" rel="tag">layout</a></li>
-          <li><a href="/tags/walks" rel="tag">walks</a></li>
-          <li><a href="/tags/gear" rel="tag">gear</a></li>
-          <li><a href="/tags/microphone" rel="tag">microphone</a></li>
-          <li><a href="/tags/self-hosting" rel="tag">self-hosting</a></li>
-          <li><a href="/tags/gitlab" rel="tag">gitlab</a></li>
-          <li><a href="/tags/autumn" rel="tag">autumn</a></li>
-          <li><a href="/tags/fog" rel="tag">fog</a></li>
-          <li><a href="/tags/queensferry" rel="tag">queensferry</a></li>
-          <li><a href="/tags/beach" rel="tag">beach</a></li>
-          <li><a href="/tags/dunblane" rel="tag">dunblane</a></li>
-          <li><a href="/tags/cathedral" rel="tag">cathedral</a></li>
-          <li><a href="/tags/church" rel="tag">church</a></li>
-          <li><a href="/tags/stirling" rel="tag">stirling</a></li>
-          <li><a href="/tags/bridge" rel="tag">bridge</a></li>
-          <li><a href="/tags/keith" rel="tag">keith</a></li>
         </ol>
       </aside>
     """
   end
 
-  def summary_bookmark(assigns) do
+  # Private components
+
+  defp content(%{type: "bookmark"} = assigns) do
     ~H"""
-      <article class="h-entry bookmark">
-        <p>
-          <span>On</span> <time class="dt-published" datetime="2013-06-13 12:00:00">Dec 2<span>,</span> 2014 <span>@</span> 5:53 am</time><span>,</span>
-          <a class="h-card p-author" href="/">I</a> <a class="u-uid u-url" href="/journal/bookmark">bookmarked a webpage</a>
-          <span>from</span> <span class="h-adr p-location"><span class="p-locality">Paris</span><span>,</span> <abbr class="p-country-name" title="France">FR</abbr></span><span>:</span>
-        </p>
-      
-        <blockquote cite="/journal/bookmark" class="e-content p-name">
-          <p><a class="u-bookmark-of" href="https://thepan-handler.com/griswold-cast-iron/" rel="external noopener">https://thepan-handler.com/griswold-cast-iron/</a></p>
-          <p>We cook on one of the 1930-1939 era Griswold pans nearly every day.</p>
-        </blockquote>
-      </article>
+      <p><a href="https://thepan-handler.com/griswold-cast-iron/" rel="external noopener">https://thepan-handler.com/griswold-cast-iron/</a></p>
+      <p>We cook on one of the 1930-1939 era Griswold pans nearly every day.</p>
+        
     """
   end
 
-  def summary_checkin(assigns) do
+  defp content(%{type: "checkin"} = assigns) do
     ~H"""
-      <article class="h-entry checkin">
-        <p>
-          <span>On</span> <time class="dt-published" datetime="2013-06-13 12:00:00">Dec 2<span>,</span> 2014 <span>@</span> 5:53 am</time><span>,</span>
-          <a class="h-card p-author" href="/">I</a> <a class="u-uid u-url" href="/journal/checkin">was at a location</a>
-          <span>in</span> <span class="h-adr p-location"><span class="p-locality">Amsterdam</span><span>,</span> <abbr class="p-country-name" title="Netherlands">NL</abbr></span><span>:</span>
-        </p>
-      
-        <blockquote cite="/journal/checkin" class="e-content p-name">
-          <p>Checked in at <span class="h-card p-name u-checkin">Schiphol Airport</span></p>
-          <p>AMS ✈ DAR</p>
-        </blockquote>
-      </article>
+      <p>Checked in at <span class="h-card p-name u-checkin">Schiphol Airport</span></p>
+      <p>AMS ✈ DAR</p>
     """
   end
 
-  def summary_note(assigns) do
+  defp content(%{type: "note"} = assigns) do
     ~H"""
-      <article class="note">
-        <p>
-          <span>On</span> <time class="dt-published" datetime="2013-06-13 12:00:00">Dec 2<span>,</span> 2014 <span>@</span> 5:53 am</time><span>,</span>
-          <a class="h-card p-author" href="/">I</a> <a class="u-uid u-url" href="/journal/note">posted a note</a>
-          <span>from</span> <span class="h-adr p-location"><span class="p-locality">Paris</span><span>,</span> <abbr class="p-country-name" title="France">FR</abbr></span><span>:</span>
-        </p>
-        <blockquote cite="/journal/6T51WU" class="e-content p-name">
-          <%= render_slot(@inner_block) %>
-        </blockquote>
-      </article>
+      <p>Nothing says “long flight to Europe” like&nbsp;Nescafé. Nothing says “long flight to Europe” like&nbsp;Nescafé.</p>
     """
   end
 
-  def summary_photo(assigns) do
+  defp content(%{type: "photo"} = assigns) do
     ~H"""
-      <article class="h-entry photo">
-        <p>
-          <span>On</span> <time class="dt-published" datetime="2013-06-13 12:00:00">Dec 2<span>,</span> 2014 <span>@</span> 5:53 am</time><span>,</span>
-          <a class="h-card p-author" href="/">I</a> <a class="u-uid u-url" href="/journal/photo">posted a photo</a>
-          <span>from</span> <span class="h-adr p-location"><span class="p-locality">Paris</span><span>,</span> <abbr class="p-country-name" title="France">FR</abbr></span><span>:</span>
-        </p>
-        <blockquote cite="/journal/6T51WU">
-          <figure>
-            <picture>
-              <source sizes="400px" srcset="https://f000.backblazeb2.com/file/jeremyboles-com/topic_journal@800w.jpg 800w, https://f000.backblazeb2.com/file/jeremyboles-com/topic_journal@400w.jpg 400w" type="image/jpeg">
-              <img alt="Mansard rooftops in Paris, France" class="u-photo" loading="lazy" src="https://f000.backblazeb2.com/file/jeremyboles-com/topic_journal@400w.jpg">
-            </picture>
-            <figcaption class="e-content p-name">
-              <p>The view from my the window outside of my apartment in&nbsp;Paris.</p>
-            </figcaption>
-          </figure>
-        </blockquote>
-      </article>
+      <figure>
+        <picture>
+          <source sizes="400px" srcset="https://f000.backblazeb2.com/file/jeremyboles-com/topic_journal@800w.jpg 800w, https://f000.backblazeb2.com/file/jeremyboles-com/topic_journal@400w.jpg 400w" type="image/jpeg">
+          <img alt="Mansard rooftops in Paris, France" class="u-photo" loading="lazy" src="https://f000.backblazeb2.com/file/jeremyboles-com/topic_journal@400w.jpg">
+        </picture>
+        <figcaption>
+          <p>The view from my the window outside of my apartment in&nbsp;Paris.</p>
+        </figcaption>
+      </figure>
     """
   end
 
-  def summary_quote(assigns) do
+  defp content(%{type: "quote"} = assigns) do
     ~H"""
-      <article class="h-entry quote">
-        <p>
-          <span>On</span> <time class="dt-published" datetime="2013-06-13 12:00:00">Dec 2<span>,</span> 2014 <span>@</span> 5:53 am</time><span>,</span>
-          <a class="h-card p-author" href="/">I</a> <a class="u-uid u-url" href="/journal/quote">posted a quote</a>
-          <span>from</span> <span class="h-adr p-location"><span class="p-locality">Paris</span><span>,</span> <abbr class="p-country-name" title="France">FR</abbr></span><span>:</span>
-        </p>
-      
-        <blockquote cite="/journal/6T51WU" class="e-content p-name">
-          <figure class="p-content">
-            <blockquote>Travel changes you. As you move through this life and this world you change things slightly, you leave marks behind, however small. And in return, life—and travel—leaves marks on&nbsp;you.</blockquote>
-            <figcaption>Anthony Bourdain, <cite>The Nasty Bits</cite></figcaption>
-          </figure>
-        </blockquote>
-      </article>
+      <figure>
+        <blockquote>Travel changes you. As you move through this life and this world you change things slightly, you leave marks behind, however small. And in return, life—and travel—leaves marks on&nbsp;you.</blockquote>
+        <figcaption>Anthony Bourdain, <cite>The Nasty Bits</cite></figcaption>
+      </figure>
     """
   end
 
-  def summary_video(assigns) do
+  defp content(%{type: "video"} = assigns) do
     ~H"""
-      <article class="h-entry video">
-        <p>
-          <span>On</span> <time class="dt-published" datetime="2013-06-13 12:00:00">Dec 2<span>,</span> 2014 <span>@</span> 5:53 am</time><span>,</span>
-          <a class="h-card p-author" href="/">I</a> <a class="u-uid u-url" href="/journal/video">posted a video</a>
-          <span>from</span> <span class="h-adr p-location"><span class="p-locality">Paris</span><span>,</span> <abbr class="p-country-name" title="France">FR</abbr></span><span>:</span>
-        </p>
-      
-        <blockquote cite="/journal/video">
-          <figure>
-            <video loop muted playsinline poster="https://f000.backblazeb2.com/file/jeremyboles-com/journal_video@240p.jpg" preload="none">
-              <source class="u-video" src="https://f000.backblazeb2.com/file/jeremyboles-com/journal_video@240p.mp4" type="video/mp4">
-              
-              <img alt="The view from inside of a car on a highway in Tanzania" src="https://f000.backblazeb2.com/file/jeremyboles-com/journal_video@240p.jpg" />
-              <p>Video downloads in h.264 format:</p>
-              <ul>
-                <li><a download href="https://f000.backblazeb2.com/file/jeremyboles-com/journal_video@240p.mp4" type="video/mp4">240p resolution</a> - 6.6 <abbr title="megabyte">MB</abbr></li>
-                <li><a download href="https://f000.backblazeb2.com/file/jeremyboles-com/journal_video@480p.mp4" type="video/mp4">480p resolution</a> - 8.9 <abbr title="megabyte">MB</abbr></li>
-                <li><a download href="https://f000.backblazeb2.com/file/jeremyboles-com/journal_video@540p.mp4" type="video/mp4">540p resolution</a> - 21.7 <abbr title="megabyte">MB</abbr></li>
-                <li><a download href="https://f000.backblazeb2.com/file/jeremyboles-com/journal_video@720p.mp4" type="video/mp4">720p resolution</a> - 42.5 <abbr title="megabyte">MB</abbr></li>
-                <li><a download href="https://f000.backblazeb2.com/file/jeremyboles-com/journal_video@1080p.mp4" type="video/mp4">1080p resolution</a> - 63.7 <abbr title="megabyte">MB</abbr></li>
-              </ul>
-            </video>
-      
-            <figcaption class="e-content p-name">
-              <p>I’ve never felt so out of place in my life.</p>
-            </figcaption>
-          </figure>
-        </blockquote>
-      </article>
+      <figure>
+        <video loop muted playsinline poster="https://f000.backblazeb2.com/file/jeremyboles-com/journal_video@240p.jpg" preload="none">
+          <source src="https://f000.backblazeb2.com/file/jeremyboles-com/journal_video@240p.mp4" type="video/mp4">
+          
+          <img alt="The view from inside of a car on a highway in Tanzania" src="https://f000.backblazeb2.com/file/jeremyboles-com/journal_video@240p.jpg" />
+          <p>Video downloads in h.264 format:</p>
+          <ul>
+            <li><a download href="https://f000.backblazeb2.com/file/jeremyboles-com/journal_video@240p.mp4" type="video/mp4">240p resolution</a> - 6.6 <abbr title="megabyte">MB</abbr></li>
+            <li><a download href="https://f000.backblazeb2.com/file/jeremyboles-com/journal_video@480p.mp4" type="video/mp4">480p resolution</a> - 8.9 <abbr title="megabyte">MB</abbr></li>
+            <li><a download href="https://f000.backblazeb2.com/file/jeremyboles-com/journal_video@540p.mp4" type="video/mp4">540p resolution</a> - 21.7 <abbr title="megabyte">MB</abbr></li>
+            <li><a download href="https://f000.backblazeb2.com/file/jeremyboles-com/journal_video@720p.mp4" type="video/mp4">720p resolution</a> - 42.5 <abbr title="megabyte">MB</abbr></li>
+            <li><a download href="https://f000.backblazeb2.com/file/jeremyboles-com/journal_video@1080p.mp4" type="video/mp4">1080p resolution</a> - 63.7 <abbr title="megabyte">MB</abbr></li>
+          </ul>
+        </video>
+
+        <figcaption>
+          <p>I’ve never felt so out of place in my life.</p>
+        </figcaption>
+      </figure>
     """
   end
 end
