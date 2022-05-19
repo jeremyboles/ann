eed4d6f1568c2455fbc16eb0b292cfd8b1d36ac6
Jeremy Boles <me@jeremyboles.com>
2022-05-19 09:33:32 -0500

    feat(bainistigh): work out the wiki sidebar will work

--- apps/bainistigh/lib/bainistigh/components/wiki_component.ex
+++ apps/bainistigh/lib/bainistigh/components/wiki_component.ex
@@ -69,7 +69,7 @@ defmodule Bainistigh.WikiComponent do
       <aside class="sidebar">
         <.sidebar_tabs />
         
-        <.metadata_panel />
+        <.metadata_panel form={@form} />
         <.glossary_panel />
         <.links_panel />
         <.history_panel />
@@ -125,10 +125,30 @@ defmodule Bainistigh.WikiComponent do
     """
   end
 
+  def location_input(assigns) do
+    ~H"""
+      <details class="location_input" open>
+        <summary>
+          <span>Location</span>
+          <button class="location-off" type="button">Location Off</button>
+        </summary>
+        
+        <div class="map" id="location-map" phx-update="ignore">
+          <div data-coords-input={input_id(:revision, :coords)} data-mapkit-response-input={input_id(:revision, :mapkit_response)} id="wiki-map" phx-hook="LocationPicker" phx-update="ignore"></div>
+          
+          <%= hidden_input :revision, :coords %>
+          <%= hidden_input :revision, :mapkit_response %>
+        </div>
+      </details>
+    """
+  end
+
   defp metadata_panel(assigns) do
     ~H"""
       <section class="metadata_panel" id="metadata-panel" role="tabpanel">
-        Metadata
+        <.topic_attributes form={@form} />
+        <.location_input  form={@form} />
+        <.tags_input form={@form} />
       </section>
     """
   end
@@ -206,6 +226,45 @@ defmodule Bainistigh.WikiComponent do
   defp sidebar_toggle(assigns) do
     ~H"""
       <input checked class="sidebar_toggle" form="none" title="Toggle Sidebar" type="checkbox" />
+    """
+  end
+
+  defp tags_input(assigns) do
+    ~H"""
+      <details class="tags_input" open>
+        <summary>
+          <span>Tags</span>
+          <button class="add-tag" phx-click="add-tag" type="button">Add Tag</button>
+        </summary>
+        
+        <ul id="wiki-tags" phx-hook="TagList">
+          <%= for {tag, index} <- Enum.with_index(input_value(@form, :tags)) do %>
+            <li>
+              <input autocomplete="off" name={"#{input_name(@form, :tags)}[]"} id={input_id(@form, :tags, index)} phx-keydown="modify-tags" phx-value-index={index} type="text" value={tag} />
+            </li>
+          <% end %>
+        </ul>
+      </details>
+    """
+  end
+
+  defp topic_attributes(assigns) do
+    ~H"""
+      <details class="topic_attributes" open>
+        <summary>Topic Attributes</summary>
+        
+        <fieldset>
+          <%= text_input @form, :url_slug, placeholder: "custom-slug" %>
+          <%= label @form, :url_slug do %>
+            <abbr title="Uniform Resource Locator">URL</abbr> Slug
+          <% end %>
+      
+          <%= text_input @form, :short_title, placeholder: " "  %>
+          <%= label @form, :short_title, "Shortened Title" %>
+      
+          <.live_component module={Bainistigh.WikiLive.ParentTopicComponent} form={@form} id="parent-topic" />
+        </fieldset>
+      </details>
     """
   end
 end
