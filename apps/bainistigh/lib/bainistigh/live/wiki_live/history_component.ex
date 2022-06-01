88bbaf9bd00609c51ebbdd7f0babf530d6b40af1
Jeremy Boles <me@jeremyboles.com>
2022-06-01 16:15:59 -0500

    fix(bainistigh): unselect the revision when the article changes

--- apps/bainistigh/lib/bainistigh/live/wiki_live/history_component.ex
+++ apps/bainistigh/lib/bainistigh/live/wiki_live/history_component.ex
@@ -12,43 +12,9 @@ defmodule Bainistigh.WikiLive.HistoryComponent do
     {:ok, assign(socket, :selected, nil)}
   end
 
-  def render(assigns) do
-    ~H"""
-      <section class="HistoryComponent" role="tabpanel">
-        <div class="history">
-          <table>
-            <thead>
-              <tr>
-                <th>Date</th>
-                <th>Location</th>
-              </tr>
-            </thead>
-            <tbody>
-              <%= if @article do %>
-                <%= for revision <- @article.revisions do %>
-                <tr aria-selected={if @selected && @selected.id == revision.id, do: "true"} phx-click="select" phx-target={@myself} phx-value-id={revision.id}>
-                    <td><%= Date.to_string(revision.updated_at) %></td>
-                    <td><%= location revision %></td>
-                  </tr>
-                <% end %>
-              <% else %>
-                <tr><td colspan="2" /></tr>
-              <% end %>
-            </tbody>
-          </table>
-        </div>
-        
-        <div class="preview">
-          <%= if @selected do %>
-            <%= raw @selected.content_html %>
-          <% end %>
-        </div>
-      </section>
-    """
-  end
-
-  defp location(%ArticleRevision{mapkit_response: []}) do
-    raw("<i>Unknown</i>")
+  def update(assigns, socket) do
+    socket = socket |> assign(assigns) |> assign(:selected, nil)
+    {:ok, socket}
   end
 
   defp location(%ArticleRevision{mapkit_response: %{"results" => [result | _]}}) do
@@ -59,5 +25,9 @@ defmodule Bainistigh.WikiLive.HistoryComponent do
       _ ->
         raw("<i>Unknown</i>")
     end
+  end
+
+  defp location(_) do
+    raw("<i>Unknown</i>")
   end
 end
