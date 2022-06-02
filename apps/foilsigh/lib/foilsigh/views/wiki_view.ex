d4336da5c06a368dee0b5603f226ebd01f57431d
Jeremy Boles <me@jeremyboles.com>
2022-06-02 04:20:59 -0500

    refactor(foilsigh): move more of the components into partials

--- apps/foilsigh/lib/foilsigh/views/wiki_view.ex
+++ apps/foilsigh/lib/foilsigh/views/wiki_view.ex
@@ -6,6 +6,9 @@ defmodule Foilsigh.WikiView do
   import Foilsigh.SharedComponent
   import Foilsigh.WikiComponent
 
+  def display_title(%Article{short_title: nil, title_text: title_text}), do: title_text
+  def display_title(%Article{short_title: short_title}), do: short_title
+
   def stylesheets("show.html", _assigns), do: ~w(/assets/templates/wiki/show.css)
   def stylesheets("index.html", _assigns), do: ~w(/assets/templates/wiki/index.css)
   def stylesheets(_action, _assigns), do: []
@@ -13,7 +16,4 @@ defmodule Foilsigh.WikiView do
   def title("index.html", _assigns), do: "Wiki · Jeremy Boles"
   def title("recipe.html", _assigns), do: "Recipe · Jeremy Boles"
   def title("show.html", %{article: article}), do: "#{article.title_text} - Wiki · Jeremy Boles"
-
-  defp display_title(%Article{short_title: nil, title_text: title_text}), do: title_text
-  defp display_title(%Article{short_title: short_title}), do: short_title
 end
