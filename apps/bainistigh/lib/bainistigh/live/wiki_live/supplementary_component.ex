26c11a39ef874e1a1c730c48ef4cc1b37aaf5bd9
Jeremy Boles <me@jeremyboles.com>
2022-06-05 06:55:50 -0500

    feat(bainistigh): get going on a design for SupplementaryComponent

--- apps/bainistigh/lib/bainistigh/live/wiki_live/supplementary_component.ex
+++ apps/bainistigh/lib/bainistigh/live/wiki_live/supplementary_component.ex
@@ -1,11 +1,3 @@
 defmodule Bainistigh.WikiLive.SupplementaryComponent do
   use Bainistigh, :live_component
-
-  def render(assigns) do
-    ~H"""
-      <section class="SupplementaryComponent">
-        supplementary
-      </section>
-    """
-  end
 end
