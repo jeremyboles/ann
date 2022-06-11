cf0dd05a232271c7dc63e467f10e32cd56745b4e
Jeremy Boles <me@jeremyboles.com>
2022-06-11 05:25:24 -0500

    feat(bainistigh): get the tag inputs hooked back to a form

--- apps/bainistigh/lib/bainistigh/live/wiki_live/tags_component.ex
+++ apps/bainistigh/lib/bainistigh/live/wiki_live/tags_component.ex
@@ -1,23 +1,38 @@
 defmodule Bainistigh.WikiLive.TagsComponent do
   use Bainistigh, :live_component
 
-  def handle_event("add-tag", _params, socket) do
-    tags =
-      case socket.assigns do
-        %{focused_index: nil} -> socket.assigns.tags ++ [""]
-        %{focused_index: index, tags: tags} -> List.insert_at(tags, index + 1, "")
-      end
+  alias Ecto.Changeset
+  alias Taifead.Topics
 
-    {:noreply, assign(socket, :tags, tags)}
+  def handle_event("add-tag", _params, %{assigns: %{changeset: changeset, index: nil}} = socket) do
+    tags = ["" | Changeset.fetch_field!(changeset, :tags)]
+    changeset = Changeset.put_change(changeset, :tags, tags)
+    {:noreply, assign(socket, :changeset, changeset)}
+  end
+
+  def handle_event("add-tag", _params, %{assigns: %{changeset: changeset, index: index}} = socket) do
+    tags = changeset |> Changeset.fetch_field!(:tags) |> List.insert_at(index + 1, "")
+    changeset = Changeset.put_change(changeset, :tags, tags)
+    {:noreply, assign(socket, :changeset, changeset)}
   end
 
   def handle_event("input-focus", %{"index" => index}, socket) do
     {index, _} = Integer.parse(index)
-    {:noreply, assign(socket, :focused_index, index)}
+    {:noreply, assign(socket, :index, index)}
+  end
+
+  def handle_event("update", %{"draft" => params}, %{assigns: %{draft: draft}} = socket) do
+    {:ok, _draft} = Topics.update_draft(draft, params)
+    {:noreply, socket}
   end
 
   def update(assigns, socket) do
-    socket = socket |> assign(assigns) |> assign(focused_index: nil, tags: [])
+    IO.inspect("UPDATE")
+    socket = socket |> assign(assigns) |> assign(index: nil) |> assign_changeset()
     {:ok, socket}
+  end
+
+  defp assign_changeset(%{assigns: %{draft: draft}} = socket) do
+    socket |> assign(:changeset, Topics.change_draft(draft))
   end
 end
