ae8d3536a84e923e7c64d9fd1bc53911ec7ec1a5
Jeremy Boles <me@jeremyboles.com>
2022-06-14 09:25:45 -0500

    feat(bainistigh): get started on a very basic design for the journal

--- apps/bainistigh/lib/bainistigh/live/journal_live.ex
+++ apps/bainistigh/lib/bainistigh/live/journal_live.ex
@@ -6,10 +6,4 @@ defmodule Bainistigh.JournalLive do
   def handle_params(_params, _url, socket), do: {:noreply, socket}
 
   def mount(_params, _session, socket), do: {:ok, assign(socket, page_title: "Journal")}
-
-  def render(assigns) do
-    ~H"""
-    <.section_header>Journal</.section_header>
-    """
-  end
 end
