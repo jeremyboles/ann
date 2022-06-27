8852ab691e5c45ab4b8fc0b1f876f9cf518e884a
Jeremy Boles <me@jeremyboles.com>
2022-06-27 09:23:44 -0500

    refactor(bainistigh): reworking how to choose the static asset path

--- apps/bainistigh/lib/bainistigh/views/layout_view.ex
+++ apps/bainistigh/lib/bainistigh/views/layout_view.ex
@@ -1,6 +1,7 @@
 defmodule Bainistigh.LayoutView do
   use Bainistigh, :view
 
+  import Bainistigh.Endpoint, only: [static_url: 0, url: 0]
   import Bainistigh.LayoutComponent
   import Phoenix.HTML.Tag
 
@@ -8,9 +9,28 @@ defmodule Bainistigh.LayoutView do
   # so we instruct Elixir to not warn if the dashboard route is missing.
   @compile {:no_warn_undefined, {Routes, :live_dashboard_path, 2}}
 
-  def manifest, do: config(:manifest)
+  def favicon_path(conn) do
+    host() <> Routes.static_path(conn, "/images/favicon.png")
+  end
 
-  defp config(key) do
-    Application.fetch_env!(:bainistigh, Bainistigh.Endpoint)[:webapp][key]
+  def javascript_path(conn) do
+    host() <> Routes.static_path(conn, "/assets/app.js")
+  end
+
+  def manifest_path(conn) do
+    manifest = Application.fetch_env!(:bainistigh, Bainistigh.Endpoint)[:webapp][:manifest]
+    host() <> Routes.static_path(conn, manifest)
+  end
+
+  def startup_image_path(conn) do
+    host() <> Routes.static_path(conn, "/images/startup.png")
+  end
+
+  def stylesheet_path(conn) do
+    host() <> Routes.static_path(conn, "/assets/all.css")
+  end
+
+  defp host do
+    if static_url() == url(), do: "", else: static_url()
   end
 end
