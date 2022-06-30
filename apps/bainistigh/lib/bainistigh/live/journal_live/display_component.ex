7eb1bac079ec89ebb84b031a80fd75fb05e1eba9
Jeremy Boles <me@jeremyboles.com>
2022-06-30 10:02:19 -0500

    feat(bainistigh): more work on getting the entry view sorted

--- apps/bainistigh/lib/bainistigh/live/journal_live/display_component.ex
+++ apps/bainistigh/lib/bainistigh/live/journal_live/display_component.ex
@@ -2,9 +2,24 @@ defmodule Bainistigh.JournalLive.DisplayComponent do
   use Bainistigh, :live_component
 
   alias Taifead.Journal
+  alias Taifead.Journal.Entry
 
   def handle_event("delete", _params, %{assigns: %{entry: entry}} = socket) do
     {:ok, _entry} = Journal.delete_entry(entry)
     {:noreply, push_patch(socket, to: "/journal")}
+  end
+
+  def update(assigns, socket) do
+    {:ok, socket = socket |> assign(assigns) |> assign_changeset()}
+  end
+
+  defp assign_changeset(socket, attrs \\ %{})
+
+  defp assign_changeset(%{assigns: %{entry: nil, kind: kind}} = socket, attrs) do
+    assign(socket, :changeset, Journal.change_entry(%Entry{kind: kind}, attrs))
+  end
+
+  defp assign_changeset(%{assigns: %{entry: entry}} = socket, attrs) do
+    assign(socket, :changeset, Journal.change_entry(entry, attrs))
   end
 end
