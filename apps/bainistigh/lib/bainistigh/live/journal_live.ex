e44ca1ff6e27d57a3e0f5e163770dbf6e7636705
Jeremy Boles <me@jeremyboles.com>
2022-07-11 12:58:47 +0100

    fix(bainistigh): don't try and print state code if there isn't one

--- apps/bainistigh/lib/bainistigh/live/journal_live.ex
+++ apps/bainistigh/lib/bainistigh/live/journal_live.ex
@@ -44,6 +44,22 @@ defmodule Bainistigh.JournalLive do
     {:ok, assign(socket, entries: entries, entry: nil, kind: nil, page_title: "Journal")}
   end
 
+  defp location(%{"muid" => _, "name" => name} = resp) do
+    raw("<b>#{name}</b>")
+  end
+
+  defp location(%{"administrativeAreaCode" => state, "country" => country, "locality" => city}) do
+    raw("<b>#{city}, #{state}, #{country}</b>")
+  end
+
+  defp location(%{"country" => country, "locality" => city}) do
+    raw("<b>#{city}, #{country}</b>")
+  end
+
+  defp location(_) do
+    raw("<b><i>Unknown Location</i></b>")
+  end
+
   defp mapkit_url(%{center: %Geo.Point{coordinates: {lng, lat}}} = opts) do
     mapkit_url(%{opts | center: "#{lat},#{lng}"})
   end
